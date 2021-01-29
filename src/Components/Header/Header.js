828820252488f8cbe6e28943d0003a85623c58f4
Zach Painter <z.painter45@gmail.com>
2021-01-29 16:46:24 -0600

    added redirect to dashboard if user is logged in

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,25 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Redirect } from "react-router-dom";
+import TokenServices from "../../Services/TokenService";
 
 export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <div className="hero">
-          <h2>Booking Made Easy</h2>
-          <p>Schedule events and get the help you need.</p>
-          <div>
-            <button>
-              <Link to="/login">Start Booking Now</Link>
-            </button>
-          </div>
-        </div>
-        <div className="hero">
-          <span className="hero-image">
-            <FontAwesomeIcon className="hero-cal" icon={faCalendarCheck} />
-          </span>
-        </div>
+        {!TokenServices.hasAuthToken() ? (
+          <>
+            <div className="hero">
+              <h2>Booking Made Easy</h2>
+              <p>Schedule events and get the help you need.</p>
+              <div>
+                <button>
+                  <Link to="/login">Start Booking Now</Link>
+                </button>
+              </div>
+            </div>
+            <div className="hero">
+              <span className="hero-image">
+                <FontAwesomeIcon className="hero-cal" icon={faCalendarCheck} />
+              </span>
+            </div>
+          </>
+        ) : (
+          <>
+            <Redirect to="/dashboard" />
+          </>
+        )}
       </header>
     );
   }
