4b7a3192c835212be9cc396ee9016337a3583b77
Zach Painter <z.painter45@gmail.com>
2021-02-06 15:02:20 -0600

    updated config and added padding to li

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  REACT_APP_API_BASE_URL: "https://shielded-cliffs-96070.herokuapp.com/api",
-  // REACT_APP_API_BASE_URL: "http://localhost:8000/api",
+  // REACT_APP_API_BASE_URL: "https://shielded-cliffs-96070.herokuapp.com/api",
+  REACT_APP_API_BASE_URL: "http://localhost:8000/api",
   INVITE_URL: "https://bookly-app.vercel.app/invite-page/",
   TOKEN_KEY: "bookly-token",
 };
